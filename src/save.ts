722acb1a3e618399b912e109f0f3e4ffcbdecdb3
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2023-12-09 00:00:18 +0000

    add some more logging and error handling

--- src/save.ts
+++ src/save.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core';
 import * as tar from 'tar';
-import * as utils from './utils';
 import * as path from 'path';
 import * as fs from 'fs';
 
@@ -10,11 +9,22 @@ async function run(): Promise<void> {
     const cachePath = core.getState('VIRTUALENV_DIRECTORY');
     const tarballPath = path.join('/caching', `${cacheKey}.tar`);
 
+    core.info(`Cache key: ${cacheKey}`);
+    core.info(`Cache path: ${cachePath}`);
+    core.info(`Tarball path: ${tarballPath}`);
+
     // Ensure the cache directory exists
     if (!fs.existsSync('/caching')) {
+      core.info('Creating /caching directory');
       fs.mkdirSync('/caching', { recursive: true });
     }
 
+    // Check if cachePath exists
+    if (!fs.existsSync(cachePath)) {
+      throw new Error(`Cache path '${cachePath}' does not exist.`);
+    }
+
+    core.info('Starting to create tarball...');
     await tar.create(
       {
         gzip: true,
@@ -24,8 +34,14 @@ async function run(): Promise<void> {
       [cachePath]
     );
 
+    // Verify that the tarball was created
+    if (!fs.existsSync(tarballPath)) {
+      throw new Error(`Failed to create tarball at '${tarballPath}'.`);
+    }
+
     core.info(`Cache saved with key: ${cacheKey}`);
   } catch (error) {
+    core.error(`An error occurred: ${(error as Error).message}`);
     core.setFailed((error as Error).message);
   }
 }
